7e20629edf120d30eac1c0b56a611538c98524f3
yesterfriday <qwe50922327@gmail.com>
2026-04-15 12:58:54 +0900

    Resolve merge conflicts from main

--- Client/MSE_BoxBox/Assets/LJC/Scripts/ScoreManager.cs
+++ Client/MSE_BoxBox/Assets/LJC/Scripts/ScoreManager.cs
@@ -1,23 +1,26 @@
-using System;
-using UnityEngine;
-
-public class ScoreManager : MonoBehaviour
+namespace LJC
 {
-    [SerializeField] private int currentScore = 0;
-
-    public int CurrentScore => currentScore;
-
-    public event Action<int> OnScoreChanged;
-
-    public void ResetScore()
+    using System;
+    using UnityEngine;
+    
+    public class ScoreManager : MonoBehaviour
     {
-        currentScore = 0;
-        OnScoreChanged?.Invoke(currentScore);
-    }
-
-    public void AddScore(int amount)
-    {
-        currentScore += amount;
-        OnScoreChanged?.Invoke(currentScore);
+        [SerializeField] private int currentScore = 0;
+    
+        public int CurrentScore => currentScore;
+    
+        public event Action<int> OnScoreChanged;
+    
+        public void ResetScore()
+        {
+            currentScore = 0;
+            OnScoreChanged?.Invoke(currentScore);
+        }
+    
+        public void AddScore(int amount)
+        {
+            currentScore += amount;
+            OnScoreChanged?.Invoke(currentScore);
+        }
     }
 }
