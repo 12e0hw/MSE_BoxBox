219a4b1de75e11a8f2a748b36001007b9f5db129
jiwoong1763-star <jiwoong1763@ajou.ac.kr>
2026-05-06 20:33:30 +0900

    login successfail panel

--- Client/MSE_BoxBox/Assets/HJW/SceneChange/ResultManager.cs
+++ Client/MSE_BoxBox/Assets/HJW/SceneChange/ResultManager.cs
@@ -2,77 +2,64 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ResultManager : MonoBehaviour
+namespace HJW.scripts
 {
-    [Header("Panels")]
-    public GameObject successPanel;     
-    public GameObject failPanel;        
-    public CanvasGroup dimCanvasGroup;
-
-    private bool isResultShowing = false;
-    
-    public float delayTime = 3.0f;
-    public int targetScore = 100;
-    private int score = 200; // 체크용
-
-
-    void Start()
+    public class ResultManager : MonoBehaviour
     {
-        successPanel.SetActive(false);
-        failPanel.SetActive(false);
-        dimCanvasGroup.gameObject.SetActive(false);
-        dimCanvasGroup.alpha = 0f;
-        isResultShowing = false;
-        
-        Time.timeScale = 1f; 
-    }
+        [Header("Panels")]
+        public GameObject successPanel; 
+        public GameObject failPanel;        
+        public CanvasGroup dimCanvasGroup;
 
-    void Update()
-    {
-        if(!isResultShowing && TimeGauage.timeOver){
-            isResultShowing = true;
-            bool isClear = (score >= targetScore);
+        public float delayTime = 3.0f;
+
+        // 페널 초기화
+        void Start()
+        {
+            successPanel.SetActive(false);
+            failPanel.SetActive(false);
+            dimCanvasGroup.gameObject.SetActive(false);
+            dimCanvasGroup.alpha = 0f;
+        }
+
+        // 까매지는 효과
+        public void StartResultSequence(bool isClear)
+        {
             StartCoroutine(ShowResultCoroutine(isClear));
         }
-    }
 
-    private IEnumerator ShowResultCoroutine(bool isClear)
-    {
-        dimCanvasGroup.gameObject.SetActive(true);
-        float timer = 0f;
+        private IEnumerator ShowResultCoroutine(bool isClear)
+        {
+            dimCanvasGroup.gameObject.SetActive(true);
+            float timer = 0f;
 
-        while (timer < delayTime)
-        {
-            timer += Time.deltaTime;
-            dimCanvasGroup.alpha = timer / delayTime; 
-            yield return null;
-        }
-        
-        if(isClear == true)
-        {
-            successPanel.SetActive(true);
-        }
-        else
-        {
-            failPanel.SetActive(true);
+            while (timer < delayTime)
+            {
+                timer += Time.deltaTime;
+                dimCanvasGroup.alpha = timer / delayTime; 
+                yield return null;
+            }
+            
+            if(isClear) successPanel.SetActive(true);
+            else failPanel.SetActive(true);
+
+            Time.timeScale = 0f; 
         }
 
-        Time.timeScale = 0f; 
-    }
+        // 씬 전환 버튼
+        public void Stage()
+        {
+            Time.timeScale = 1f;
+            ChangeManager.stageSelectMemo = true;
+            SceneManager.LoadScene("MainScene");
+        }
 
-
-    public void Stage()
-    {
-        Time.timeScale = 1f;
-        ChangeManager.stageSelectMemo = true;
-        SceneManager.LoadScene("MainScene");
-    }
-
-    public void LeaderBoard()
-    {
-        ChangeManager.leaderboardMemo = true;
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("MainScene"); 
+        public void LeaderBoard()
+        {
+            ChangeManager.leaderboardMemo = true;
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("MainScene"); 
+        }
     }
 }
 
