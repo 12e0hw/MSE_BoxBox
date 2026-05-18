06db770da497848d5bc4238cf90f1528b7191080
jiwoong1763-star <jiwoong1763@ajou.ac.kr>
2026-05-18 16:37:10 +0900

    Setting controlkey

---
+++ Client/MSE_BoxBox/Assets/HJW/Scripits/SettingChangeManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingChangeManager : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject SettingPanel;
+    public GameObject ControlKey1Panel;
+    public GameObject ControlKey2Panel;
+
+    void Start()
+    {
+        if(ControlKey1Panel != null) ControlKey1Panel.SetActive(false);
+        if(ControlKey2Panel != null) ControlKey2Panel.SetActive(false);
+    }
+
+    public void Back()
+    {
+        if(SettingPanel != null) SettingPanel.SetActive(false);
+        if(ControlKey1Panel != null) ControlKey1Panel.SetActive(false);
+        if(ControlKey2Panel != null) ControlKey2Panel.SetActive(false);
+    }
+
+    public void BacktoSetting()
+    {
+        if(ControlKey1Panel != null) ControlKey1Panel.SetActive(false);
+        if(ControlKey2Panel != null) ControlKey2Panel.SetActive(false);
+    }
+
+    public void ControlKey1()
+    {
+        ControlKey1Panel.SetActive(true);
+    }
+
+    public void ControlKey2()
+    {
+        ControlKey2Panel.SetActive(true);
+    }
+}
