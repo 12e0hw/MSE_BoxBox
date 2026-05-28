59aa17f385c1e7b9fbb7ed3d1af2de1d3b035f40
jiwoong1763-star <jiwoong1763@ajou.ac.kr>
2026-05-28 14:03:00 +0900

    Revert "키세팅 재설정"
    This reverts commit a2e738d6baf7188a5b3d4d362ecce6b48157931f.

--- Client/MSE_BoxBox/Assets/HJW/Scripits/ChangeManager.cs
+++ Client/MSE_BoxBox/Assets/HJW/Scripits/ChangeManager.cs
@@ -113,8 +113,10 @@ public class ChangeManager : MonoBehaviour
     public void OpenSettings()
     {
         if (SettingPanel != null) SettingPanel.SetActive(true);
+        
         if (LoginPanel != null) LoginPanel.SetActive(false);
         if (SignupPanel != null) SignupPanel.SetActive(false);
+        if (StageSelectPanel != null) StageSelectPanel.SetActive(false);
         if (LeaderboardPanel != null) LeaderboardPanel.SetActive(false);
     }
 
@@ -137,16 +139,6 @@ public class ChangeManager : MonoBehaviour
         if (SignupPanel != null) SignupPanel.SetActive(false);
         if (StageSelectPanel != null) StageSelectPanel.SetActive(false);
         if (LeaderboardPanel != null) LeaderboardPanel.SetActive(false);
-    }
-
-    public void BacktoMain()
-    {
-        if (LoginPanel != null) LoginPanel.SetActive(false);
-        if (SettingPanel != null) SettingPanel.SetActive(false);
-        if (SignupPanel != null) SignupPanel.SetActive(false);
-        if (StageSelectPanel != null) StageSelectPanel.SetActive(false);
-        if (LeaderboardPanel != null) LeaderboardPanel.SetActive(false);
-        if (StageSelectPanel!= null) StageSelectPanel.SetActive(false);
     }
 
     public void BacktoLogin()
